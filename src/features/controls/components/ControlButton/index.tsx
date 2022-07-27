aaa0dc67981d8f3fe7c5cde4dfb9252fb237985c
Andry <andrysitrakiniainasoa@gmail.com>
2022-07-27 08:36:05 +0300

    add active status to the control buttons

--- src/features/controls/components/ControlButton/index.tsx
+++ src/features/controls/components/ControlButton/index.tsx
@@ -1,14 +1,19 @@
-import { FC } from "react";
+import { FC, useState } from "react";
+import "./style.css";
 
 interface ControlButtonProps {
   text: string | number;
 }
 
 const ControlButton: FC<ControlButtonProps> = ({ text }) => {
+  const [isActive, setIsActive] = useState(false);
+  const toggleIsActive = () => setIsActive((active) => !active);
+
   return (
     <button
-      style={{ width: "3rem", height: "3rem" }}
-      className="d-inline-block rounded-circle"
+      onClick={toggleIsActive}
+      className={`${isActive ? "active" : ""}
+        shadow-sm control d-inline-block rounded-circle border border-4 border-secondary`}
     >
       {text}
     </button>
