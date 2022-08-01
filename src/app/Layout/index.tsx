26707f76a1d6b4a57ae34897b8d4ef828bd3da8f
Andry <andrysitrakiniainasoa@gmail.com>
2022-08-01 09:19:44 +0300

    update gap for main app

--- src/app/Layout/index.tsx
+++ src/app/Layout/index.tsx
@@ -6,8 +6,8 @@ type AppLayoutProps = ComponentWithChildren;
 const AppLayout: FC<AppLayoutProps> = ({ children }) => {
   return (
     <div
-      className="App p-4 d-flex align-items-end gap-4"
-      style={{ minHeight: "100vh" }}
+      className="App p-4 d-flex align-items-end"
+      style={{ minHeight: "100vh", gap: "6rem" }}
     >
       {children}
     </div>
